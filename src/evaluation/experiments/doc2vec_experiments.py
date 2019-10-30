5204280050a411ca42c0326d66f9396680121138
Daniel McCrystal <d.mccrystal512@gmail.com>
2019-10-30 10:14:08 -0400

    added use_negatives parameter to doc2vec.py

--- src/evaluation/experiments/doc2vec_experiments.py
+++ src/evaluation/experiments/doc2vec_experiments.py
@@ -1,33 +1,47 @@
 import sys
 sys.path.append('../..')
 
-from evaluation.Corpus import Corpus
-from evaluation.doc2vec import Doc2Vec_IR
-from evaluation.Evaluator import Evaluator
 from evaluation.VSM import VSM
+from evaluation.Evaluator import Evaluator
+from evaluation.doc2vec import Doc2Vec_IR
+from evaluation.Corpus import Corpus
+
+
 
 corpus = Corpus.get_preset_corpus('0_1')
 vsm_generator = VSM(corpus)
 vsm_model = vsm_generator.generate_model()
 
+
 def vector_size_test():
     doc2vec_generator = Doc2Vec_IR(corpus)
-    doc2vec_model_0 = doc2vec_generator.generate_model(parameters={'vector_size': 200})
-    doc2vec_model_1 = doc2vec_generator.generate_model(parameters={'vector_size': 300})
-    doc2vec_model_2 = doc2vec_generator.generate_model(parameters={'vector_size': 400})
+    doc2vec_model_0 = doc2vec_generator.generate_model(
+        parameters={'vector_size': 200})
+    doc2vec_model_1 = doc2vec_generator.generate_model(
+        parameters={'vector_size': 300})
+    doc2vec_model_2 = doc2vec_generator.generate_model(
+        parameters={'vector_size': 400})
 
-    evaluator = Evaluator([vsm_model, doc2vec_model_0, doc2vec_model_1, doc2vec_model_2], corpus)
+    evaluator = Evaluator(
+        [vsm_model, doc2vec_model_0, doc2vec_model_1, doc2vec_model_2], corpus)
     evaluator.precision_recall(show_parameters=True, show_random_model=True)
+
 
 def epochs_test():
     doc2vec_generator = Doc2Vec_IR(corpus)
-    doc2vec_model_0 = doc2vec_generator.generate_model(parameters={'epochs': 25})
-    doc2vec_model_1 = doc2vec_generator.generate_model(parameters={'epochs': 50})
-    doc2vec_model_2 = doc2vec_generator.generate_model(parameters={'epochs': 75})
-    doc2vec_model_3 = doc2vec_generator.generate_model(parameters={'epochs': 100})
+    doc2vec_model_0 = doc2vec_generator.generate_model(
+        parameters={'epochs': 25})
+    doc2vec_model_1 = doc2vec_generator.generate_model(
+        parameters={'epochs': 50})
+    doc2vec_model_2 = doc2vec_generator.generate_model(
+        parameters={'epochs': 75})
+    doc2vec_model_3 = doc2vec_generator.generate_model(
+        parameters={'epochs': 100})
 
-    evaluator = Evaluator([vsm_model, doc2vec_model_0, doc2vec_model_1, doc2vec_model_2, doc2vec_model_3], corpus)
+    evaluator = Evaluator([vsm_model, doc2vec_model_0,
+                           doc2vec_model_1, doc2vec_model_2, doc2vec_model_3], corpus)
     evaluator.precision_recall(show_parameters=True, show_random_model=True)
+
 
 def shared_vocab_test():
     doc2vec_generator0 = Doc2Vec_IR(corpus)
@@ -35,7 +49,20 @@ def shared_vocab_test():
     doc2vec_model_0 = doc2vec_generator0.generate_model()
     doc2vec_model_1 = doc2vec_generator1.generate_model()
 
-    evaluator = Evaluator([vsm_model, doc2vec_model_0, doc2vec_model_1], corpus)
+    evaluator = Evaluator(
+        [vsm_model, doc2vec_model_0, doc2vec_model_1], corpus)
+    evaluator.precision_recall(show_parameters=True, show_random_model=True)
+
+
+def use_negative_test():
+    doc2vec_generator = Doc2Vec_IR(corpus)
+    doc2vec_model_0 = doc2vec_generator.generate_model(
+        parameters={'use_negatives': False})
+    doc2ved_model_1 = doc2vec_generator.generate_model(
+        parameters={'use_negatives': True})
+
+    evaluator = Evaluator(
+        [vsm_model, doc2vec_model_0, doc2ved_model_1], corpus)
     evaluator.precision_recall(show_parameters=True, show_random_model=True)
 
 
@@ -47,8 +74,9 @@ def simple_test():
     evaluator.precision_recall(show_parameters=True, show_random_model=True)
 
 
-simple_test()
-#vector_size_test()
-#epochs_test()
-#shared_vocab_test()
-#similarity_metric_test()
+# simple_test()
+use_negative_test()
+# vector_size_test()
+# epochs_test()
+# shared_vocab_test()
+# similarity_metric_test()
