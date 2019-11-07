22c03d6e9e673e769cec1d0cf6e383744638d586
Daniel McCrystal <d.mccrystal512@gmail.com>
2019-11-07 10:04:03 -0500

    setup bpe branch with gregs code

--- src/evaluation/IR_Method.py
+++ src/evaluation/IR_Method.py
@@ -3,16 +3,14 @@ Daniel McCrystal
 June 2018
 
 '''
+from nltk import word_tokenize
+import sentencepiece as spm
+import re
+from evaluation.Trace_Model import Trace_Model
+import os.path
 import sys
 sys.path.append('../..')
 
-import os.path
-
-from evaluation.Trace_Model import Trace_Model
-
-import re
-
-from nltk import word_tokenize
 
 class IR_Method:
     """
@@ -45,14 +43,16 @@ class IR_Method:
     EXISTING_METHODS = []
 
     def __init__(self, corpus,
-        only_alphnum=False,
-        only_alph=True,
-        split_camel_case=True,
-        split_snake_case=True,
-        remove_stop_words=True,
-        stem=True,
-        only_common_vocab=True,
-        relationship_type=0):
+                 only_alphnum=False,
+                 only_alph=True,
+                 split_camel_case=True,
+                 split_snake_case=True,
+                 remove_stop_words=True,
+                 stem=True,
+                 only_common_vocab=True,
+                 bpe=False,
+                 bpe_vocab_size=2000,
+                 relationship_type=0):
         """
         Attributes:
             corpus (Corpus): The corpus object that this IR object will evaluate
@@ -105,6 +105,11 @@ class IR_Method:
             non_default_arguments['stemming'] = stem
         if only_common_vocab != True:
             non_default_arguments['only_common_vocab'] = only_common_vocab
+        if bpe != False:
+            non_default_arguments['bpe'] = bpe
+        if bpe_vocab_size != 2000:
+            non_default_arguments['bpe_vocab_size'] = bpe_vocab_size
+
         self._non_default_parameters = non_default_arguments
 
         self._only_alphnum = only_alphnum
@@ -114,13 +119,14 @@ class IR_Method:
         self._remove_stop_words = remove_stop_words
         self._stem = stem
         self._only_common_vocab = only_common_vocab
+        self._bpe = bpe
+        self._bpe_vocab_size = bpe_vocab_size
 
         for method in IR_Method.EXISTING_METHODS:
             if self.has_same_preprocessing_params(method):
                 print("Reusing existing preprocessed artifacts")
                 self._source_vocab = method._source_vocab
                 self._target_vocab = method._target_vocab
-
 
                 self._processed_sources = method._processed_sources
                 self._processed_targets = method._processed_targets
@@ -132,7 +138,8 @@ class IR_Method:
                     self._processed_sources = method._processed_targets
                     self._source_vocab = method._target_vocab
 
-                self._common_vocab = self._source_vocab.intersection(self._target_vocab)
+                self._common_vocab = self._source_vocab.intersection(
+                    self._target_vocab)
                 break
         else:
             self._preprocess()
@@ -146,20 +153,52 @@ class IR_Method:
             None
         """
 
-        tokenized_processed_sources = self._processed_artifacts(self._corpus.get_sources(), self._source_vocab)
-        tokenized_processed_targets = self._processed_artifacts(self._corpus.get_targets(), self._target_vocab)
+        if self._bpe:
 
-        if self._relationship_type == 1:
-            tokenized_processed_targets = tokenized_processed_sources
-            self._target_vocab = self._source_vocab
-        elif self._relationship_type == 2:
-            tokenized_processed_sources = tokenized_processed_targets
-            self._source_vocab = self._target_vocab
+            corpus_code_list = self._corpus.get_corpus_code().split(' ')
+            corpus_code = corpus_code_list[-1].strip('(').strip(')')
 
-        self._common_vocab = self._source_vocab.intersection(self._target_vocab)
+            corpus_name_list = self._corpus.get_corpus_name().split(' ')
+            corpus_name = corpus_name_list[0]
 
-        self._processed_sources = [[x for x in source_artifact if (not self._only_common_vocab or x in self._common_vocab)] for source_artifact in tokenized_processed_sources]
-        self._processed_targets = [[x for x in target_artifact if (not self._only_common_vocab or x in self._common_vocab)] for target_artifact in tokenized_processed_targets]
+            file_path = os.path.dirname(os.path.abspath(__file__))
+            corpus_path = file_path + '/../../data/raw/' + corpus_name + \
+                '_semeru_format/' + corpus_code + '_raw_corpus.txt'
+
+            param_str = '--input=' + corpus_path + ' --model_prefix=m_bpe --vocab_size=' + \
+                str(self._bpe_vocab_size) + ' --model_type=bpe'
+            spm.SentencePieceTrainer.train(param_str)
+            sp_bpe = spm.SentencePieceProcessor()
+
+            self._processed_sources = self._bpe_processed_artifacts(
+                sp_bpe, sources=True)
+            self._processed_targets = self._bpe_processed_artifacts(
+                sp_bpe, sources=False)
+
+            print(self._processed_sources)
+            print(self._processed_targets)
+
+        else:
+
+            tokenized_processed_sources = self._processed_artifacts(
+                self._corpus.get_sources(), self._source_vocab)
+            tokenized_processed_targets = self._processed_artifacts(
+                self._corpus.get_targets(), self._target_vocab)
+
+            if self._relationship_type == 1:
+                tokenized_processed_targets = tokenized_processed_sources
+                self._target_vocab = self._source_vocab
+            elif self._relationship_type == 2:
+                tokenized_processed_sources = tokenized_processed_targets
+                self._source_vocab = self._target_vocab
+
+            self._common_vocab = self._source_vocab.intersection(
+                self._target_vocab)
+
+            self._processed_sources = [[x for x in source_artifact if (
+                not self._only_common_vocab or x in self._common_vocab)] for source_artifact in tokenized_processed_sources]
+            self._processed_targets = [[x for x in target_artifact if (
+                not self._only_common_vocab or x in self._common_vocab)] for target_artifact in tokenized_processed_targets]
 
     def _split_camel_case_token(self, token):
         return re.sub('([a-z])([A-Z])', r'\1 \2', token).split()
@@ -169,6 +208,23 @@ class IR_Method:
 
     def _join_lists(self, lists):
         return [x for y in lists for x in y if len(x) > 0]
+
+    def _bpe_processed_artifacts(self, bpe_model, sources):
+        bpe_model.load('m_bpe.model')
+        out = []
+
+        if sources:
+            artifacts = self._corpus.get_sources()
+        else:
+            artifacts = self._corpus.get_targets()
+
+        for artifact in artifacts:
+            split_artifact = artifact.split('\n')
+            for line in split_artifact:
+                encoded = bpe_model.encode_as_pieces(line)
+                out.append(encoded)
+
+        return out
 
     def _processed_artifacts(self, artifacts, vocabulary):
         """
@@ -195,32 +251,37 @@ class IR_Method:
 
                 potential_new_tokens = [token]
 
-                potential_new_tokens = self._join_lists([re.split('[-,./]', t) for t in potential_new_tokens])
+                potential_new_tokens = self._join_lists(
+                    [re.split('[-,./]', t) for t in potential_new_tokens])
 
                 if self._only_alph:
                     # Remove non alphabetical characters
-                    potential_new_tokens = [re.sub(r'[^a-zA-z_]', '', t) for t in potential_new_tokens]
+                    potential_new_tokens = [
+                        re.sub(r'[^a-zA-z_]', '', t) for t in potential_new_tokens]
 
                 elif self._only_alphnum:
                     # Remove non alpha-numeric characters
-                    potential_new_tokens = [re.sub(r'[^a-zA-Z0-9_]', '', t) for t in potential_new_tokens]
+                    potential_new_tokens = [
+                        re.sub(r'[^a-zA-Z0-9_]', '', t) for t in potential_new_tokens]
 
                 if self._split_camel_case:
                     # Split instances of camel case
                     potential_new_tokens = self._join_lists([self._split_camel_case_token(t)
-                        for t in potential_new_tokens])
+                                                             for t in potential_new_tokens])
 
                 if self._split_snake_case:
                     # Split instances of snake case
                     potential_new_tokens = self._join_lists([self._split_snake_case_token(t)
-                        for t in potential_new_tokens])
+                                                             for t in potential_new_tokens])
 
                 # Make all tokens lowercase
-                potential_new_tokens = [t.lower() for t in potential_new_tokens]
+                potential_new_tokens = [t.lower()
+                                        for t in potential_new_tokens]
 
                 if self._remove_stop_words:
                     # Remove stop words
-                    potential_new_tokens = [t for t in potential_new_tokens if t not in self._corpus.get_stop_words()]
+                    potential_new_tokens = [
+                        t for t in potential_new_tokens if t not in self._corpus.get_stop_words()]
 
                 if self._stem:
                     # Get stem of words
@@ -255,8 +316,8 @@ class IR_Method:
         elif self._relationship_type == 2:
             sources = targets
 
-        model= Trace_Model(name, self._corpus.get_corpus_name(),
-            sources, targets, all_parameters)
+        model = Trace_Model(name, self._corpus.get_corpus_name(),
+                            sources, targets, all_parameters)
 
         return model
 
