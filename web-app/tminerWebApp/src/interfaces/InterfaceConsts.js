8ea6869d88b47b9d6ec1253c51116559ef0583ca
root <johnr@dominionintegration.com>
2020-10-27 15:35:03 -0400

    New Commit, Same Great Issue #60!

--- web-app/tminerWebApp/src/interfaces/InterfaceConsts.js
+++ web-app/tminerWebApp/src/interfaces/InterfaceConsts.js
@@ -1,5 +1,5 @@
 const NPM_DATA_LOCATION = 'libest/';
-const FLASK_DATA_LOCATION = 'libest';
+const FLASK_DATA_LOCATION = 'tminer/libest';
 
 export const DATA_LOCATION = FLASK_DATA_LOCATION;
 export const PROJECT_KEY = 'libest';
