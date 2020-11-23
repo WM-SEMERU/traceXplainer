e712edf80da07d833b07db7a21968b4a6fadda01
Jade Chen <jadechen2@gmail.com>
2020-11-23 18:01:42 -0500

    Updated fields and added mount method to update state

--- web-app/tminerWebApp/src/AnalysisView/TargetBrowser.jsx
+++ web-app/tminerWebApp/src/AnalysisView/TargetBrowser.jsx
@@ -9,19 +9,81 @@ import { getAnalysisMetrics } from '../interfaces/ArtifactInterface'; //ADDED
 export default class TargetBrowser extends React.Component {
 
 	state = {
-		artifactInfos: null,
-		//ADDED BELOW
-                numberDocs: 0,
+		numberDocs: 0,
+		numberDocs_diff: 0,
 		vocabSize: 0,
-		averageNumTokens: 0,
-		reqVocab: 0,
-		srcVocab: 0,
-		sharedVocab: 0,
+		vocabSize_diff: 0,
+		avgNumTokens: 0,
+		avgNumTokens_diff: 0,
+		vocab1: "",
+		vocab1_count: 0,
+		vocab1_freq: 0,
+		vocab2: "",
+		vocab2_count: 0,
+		vocab2_freq: 0,
+		vocab3: "",
+		vocab3_count: 0,
+		vocab3_freq: 0
+	}
+
+	convertDiffToString(num) {
+		var diff = "";
+		if (num > 0) {
+			diff = "+" + num;
+		}
+		else {
+			diff = "" + num;
+		}
+		return diff;
 	}
 
 	currentArtifactClass = 'req';
 
-	constructor(props) {
+	componentDidMount(currentArtifactClass) {
+		getAnalysisMetrics().then((analysisMetrics) => {
+
+			var index = 0;
+			var vocab = analysisMetrics.rec_vocab;
+			if (currentArtifactClass == "src") {
+				index = 1;
+				vocab = analysisMetrics.src_vocab;
+			}
+
+			var v1 = Object.keys(vocab)[0];
+			var v2 = Object.keys(vocab)[1];
+			var v3 = Object.keys(vocab)[2];
+			var v1_ct = (vocab)[v1][0];
+			var v1_fr = ((vocab)[v1][1]).toFixed(10);
+			var v2_ct = (vocab)[v2][0];
+			var v2_fr = ((vocab)[v2][1]).toFixed(10);
+			var v3_ct = (vocab)[v3][0];
+			var v3_fr = ((vocab)[v3][1]).toFixed(10);
+
+			this.setState({
+
+				numberDocs: analysisMetrics.num_doc[index],
+				numberDocs_diff: this.convertDiffToString(analysisMetrics.num_doc[3]),
+				vocabSize: analysisMetrics.vocab_size[index],
+				vocabSize_diff: this.convertDiffToString(analysisMetrics.vocab_size[3]),
+				avgNumTokens: (analysisMetrics.avg_tokens[index]).toFixed(10),
+				avgNumTokens_diff: this.convertDiffToString((analysisMetrics.avg_tokens[3]).toFixed(10)),
+
+				vocab1: v1,
+				vocab2: v2,
+				vocab3: v3,
+
+				vocab1_count: v1_ct,
+				vocab2_count: v2_ct,
+				vocab3_count: v3_ct,
+
+				vocab1_freq: v1_fr,
+				vocab2_freq: v2_fr,
+				vocab3_freq: v3_fr
+			});
+		});
+	}
+
+/*	constructor(props) {
 		super(props);
 
 		this.artifactCardRefs = [];
@@ -33,7 +95,7 @@ export default class TargetBrowser extends React.Component {
 		};
 
 		this.artifactCardRefs = artifactInfos.map((artifactInfo) => React.createRef());
-	}
+	}*/
 
 	render() {
 		return (
