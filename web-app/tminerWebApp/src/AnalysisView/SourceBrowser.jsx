4ffa2005f0d06b9faa97379b090345ff00f96271
Jade Chen <jadechen2@gmail.com>
2020-11-23 16:42:55 -0500

    Added method to add "+" for difference and added componentDidMount to update state

--- web-app/tminerWebApp/src/AnalysisView/SourceBrowser.jsx
+++ web-app/tminerWebApp/src/AnalysisView/SourceBrowser.jsx
@@ -27,7 +27,57 @@ export default class SourceBrowser extends React.Component {
 		vocab3_freq: 0
 	}
 
-	currentArtifactClass = 'req';
+	convertDiffToString(num) {
+		var diff = "";
+		if (num > 0) {
+			diff = "+" + num;
+		}
+		else {
+			diff = "" + num;
+		}
+		return diff;
+	}
+
+	//RETRIEVE DATA
+	componentDidMount() {
+		getAnalysisMetrics().then((analysisMetrics) => {
+			var numDoc_diff = this.convertDiffToString(analysisMetrics.num_doc[2]);
+			var vocab = analysisMetrics.src_vocab;
+			var v1 = Object.keys(vocab)[0];
+			var v2 = Object.keys(vocab)[1];
+			var v3 = Object.keys(vocab)[2];
+			var v1_ct = (vocab)[v1][0];
+			var v1_fr = ((vocab)[v1][1]).toFixed(10);
+			var v2_ct = (vocab)[v2][0];
+			var v2_fr = ((vocab)[v2][1]).toFixed(10);
+			var v3_ct = (vocab)[v3][0];
+			var v3_fr = ((vocab)[v3][1]).toFixed(10);
+
+			this.setState({
+				// set source fields
+				numberDocs: analysisMetrics.num_doc[0],
+				numberDocs_diff: this.convertDiffToString(analysisMetrics.num_doc[2]),
+				vocabSize: analysisMetrics.vocab_size[0],
+				vocabSize_diff: this.convertDiffToString(analysisMetrics.vocab_size[2]),
+				avgNumTokens: (analysisMetrics.avg_tokens[0]).toFixed(10),
+				avgNumTokens_diff: this.convertDiffToString((analysisMetrics.avg_tokens[2]).toFixed(10)),
+				// set vocab
+				vocab1: v1,
+				vocab2: v2,
+				vocab3: v3,
+				// set vocab counts
+				vocab1_count: v1_ct,
+				vocab2_count: v2_ct,
+				vocab3_count: v3_ct,
+				// set vocab frequencies
+				vocab1_freq: v1_fr,
+				vocab2_freq: v2_fr,
+				vocab3_freq: v3_fr
+			});
+		});
+	}
+
+/*	currentArtifactClass = 'req';
 
 	constructor(props) {
 		super(props);
@@ -41,7 +91,7 @@ export default class SourceBrowser extends React.Component {
 		};
 
 		this.artifactCardRefs = artifactInfos.map((artifactInfo) => React.createRef());
-	}
+	}*/
 
 	deselectCurrentlySelectedArtifact() {
 
