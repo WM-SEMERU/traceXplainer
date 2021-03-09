3d408b1f772144e05ccfe899283be2a4f1b6f2ec
roger <rmclanton@email.wm.edu>
2021-03-09 10:30:55 -0500

    Updated Docker and start.sh. Tested importing and using ds4se package.

--- Dash-Python/apps/app2.py
+++ Dash-Python/apps/app2.py
@@ -1,26 +1,37 @@
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table
 from dash.dependencies import Input, Output, State
 from dash.exceptions import PreventUpdate
 
 from app import app
 
-layout = html.Div([
-    html.H3('App 2'),
-    dcc.Dropdown(
-        id='app-2-dropdown',
-        options=[
-            {'label': 'App 2 - {}'.format(i), 'value': i} for i in [
-                'NYC', 'MTL', 'LA'
-            ]
-        ]
-    ),
-    html.Div(id='app-2-display-value'),
-    dcc.Link('Go to App 1', href='/apps/app1'),
-    html.Button(["Display data"], id="display-button2"),
-    dcc.Textarea(value="", id="test-out2")
-])
+from ds4se.ds.description.eval.traceability import VectorEvaluation
 
+
+def libest_params():
+    return {
+        "system": 'libest',
+        "experiment_path_w2v": "artifacts/[libest-VectorizationType.word2vec-LinkType.req2tc-True-1608690009.09251].csv",
+        "experiment_path_d2v": "artifacts/[libest-VectorizationType.doc2vec-LinkType.req2tc-True-1609289141.142806].csv",
+        'saving_path': 'testbeds/processed/',
+        'system_long': 'libest',
+        'timestamp': 1596063103.098236,
+        'language': 'all-corpus'
+    }
+
+
+params = libest_params()
+v = VectorEvaluation(params)
+
+sharedInfoTable = dash_table.DataTable(
+            id="shared_info_datatable",
+            data=v.sharedInfo.to_dict("records"),
+            page_current=0,
+            sort_action='native',
+            columns=[{'id': c, 'name': c} for c in v.sharedInfo.columns],
+            filter_action="native",
+            page_size=10, )
 
 @app.callback(
     Output('app-2-display-value', 'children'),
@@ -43,3 +54,20 @@ def on_click3(n_clicks, data):
     for key in data.keys():
         s += data[key] + "\n"
     return s
+
+layout = html.Div([
+    html.H3('App 2'),
+    dcc.Dropdown(
+        id='app-2-dropdown',
+        options=[
+            {'label': 'App 2 - {}'.format(i), 'value': i} for i in [
+                'NYC', 'MTL', 'LA'
+            ]
+        ]
+    ),
+    html.Div(id='app-2-display-value'),
+    dcc.Link('Go to App 1', href='/apps/app1'),
+    html.Button(["Display data"], id="display-button2"),
+    dcc.Textarea(value="", id="test-out2"),
+    sharedInfoTable
+])
---
+++ Dash-Python/ds4se-test.py
@@ -0,0 +1,25 @@
+import ds4se
+from ds4se.ds.description.eval.traceability import VectorEvaluation
+
+#experiment = 'experiments0.0.x/'
+#path_data = '../ds4se/dvc-ds4se/metrics/traceability/' + experiment
+
+
+def libest_params():
+    return {
+        "system": 'libest',
+        #"experiment_path_w2v": path_data + '[libest-VectorizationType.word2vec-LinkType.req2tc-True-1609292406.653621].csv',
+        #"experiment_path_d2v": path_data + '[libest-VectorizationType.doc2vec-LinkType.req2tc-True-1609289141.142806].csv',
+        "experiment_path_w2v": "artifacts/[libest-VectorizationType.word2vec-LinkType.req2tc-True-1608690009.09251].csv",
+        "experiment_path_d2v": "artifacts/[libest-VectorizationType.doc2vec-LinkType.req2tc-True-1609289141.142806].csv",
+        #'saving_path': '../dvc-ds4se/se-benchmarking/traceability/testbeds/processed/',
+        'saving_path': 'testbeds/processed/',
+        'system_long': 'libest',
+        'timestamp': 1596063103.098236,
+        'language': 'all-corpus'
+    }
+
+
+params = libest_params()
+v = VectorEvaluation(params)
+print(type(v.sharedInfo))
