08f5be4bb1e61719cc839ba7e85289b0e44afaeb
roger <rmclanton@email.wm.edu>
2021-03-13 17:42:50 -0500

    Added sidebar for navigation. Causes id not found in layout errors on load, though the app still has the expected behavior.

--- Dash-Python/index.py
+++ Dash-Python/index.py
@@ -1,16 +1,59 @@
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input, Output
 
 from app import app
 from apps import app1, app2, main, fileUploadPage
 
+# the style arguments for the sidebar. We use position:fixed and a fixed width
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
 
-app.layout = html.Div([
-    dcc.Store(id='local', storage_type='local'),
-    dcc.Location(id='url', refresh=False),
-    html.Div(id='page-content')
-])
+# the styles for the main content position it to the right of the sidebar and
+# add some padding.
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+sidebar = html.Div(
+    [
+        html.H2("Sidebar", className="display-4"),
+        html.Hr(),
+        html.P(
+            "A simple sidebar layout with navigation links", className="lead"
+        ),
+        dbc.Nav(
+            [
+                dbc.NavLink("Home", href="/", active="exact"),
+                dbc.NavLink("Page 1", href="/apps/app1", active="exact"),
+                dbc.NavLink("Page 2", href="/apps/app2", active="exact"),
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+content = html.Div(id="page-content", style=CONTENT_STYLE)
+
+app.layout = html.Div([dcc.Store(id='local', storage_type='local'),dcc.Location(id="url"), sidebar, content],    )
+
+# app.layout = html.Div([
+#     dcc.Store(id='local', storage_type='local'),
+#     dcc.Location(id='url', refresh=False),
+#     html.Div(id='page-content')
+# ])
 
 
 @app.callback(Output('page-content', 'children'),
